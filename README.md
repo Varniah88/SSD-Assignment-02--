23b07758bd38d58879001919b44a31b41ee1c09e
Varniah88 <jaanuvarni@gmail.com>
2021-09-27 11:29:46 +0530

    SSD Assignment Initial Commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# SSD-Assignment-2
+
+IT18001976
+
+Step One: Install the dependencies
+
+`npm install`
+
+Step Two: To run the application
+
+Development
+`npm run dev`
+Production
+`npm run prod`
---
+++ configs/keys.js
@@ -0,0 +1,12 @@
+const KEYS = {
+  googleOauth: {
+    clientID:
+      "569518909018-nrjb8dne31n8clen34qu2va0jlrshu6k.apps.googleusercontent.com",
+    clientSecret: "efUGGQuwqKNaDMGbYWRdJNrx",
+    callback: "/auth/google/redirect",
+  },
+  secret_key: "SECRET",
+  scope_option: "https://www.googleapis.com/auth/drive.file",
+};
+
+module.exports = KEYS;
